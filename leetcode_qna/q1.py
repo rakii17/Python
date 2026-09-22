099af74d3958e84df2ddf677c5179f71e9d2e1b9
Rakesh <rakesh.717.02@gmail.com>
2026-09-22 16:00:25 +0530

    Create q1.py

---
+++ leetcode_qna/q1.py
@@ -0,0 +1,25 @@
+'''1. Two Sum
+You are given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Question Link: https://leetcode.com/problem-list/w0f2z5yj/ '''
+
+#Solution1:
+for i,v in enumerate(nums):
+            for j in range(i + 1, len(nums)):
+                if v + nums[j] == target:
+                    return [i,j]
+
+#Solution2:
+seen = {}
+
+for i in range(len(nums)):
+    needed = target - nums[i]
+
+    if needed in seen:
+        return [seen[needed], i]
+
+    seen[nums[i]] = i
+
+          
+# Output:
+# [0,1]
